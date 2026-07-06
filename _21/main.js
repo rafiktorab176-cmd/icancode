d1b8c207e5f8f3e8988582ef787be38c23754a8d
Rafik <rafiktorab176@gmail.com>
2026-07-06 18:52:23 +0300

    Add initial project files including HTML, CSS, and JavaScript

---
+++ _21/main.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+  document.querySelector("h1").style.color = "blue";
+};
+window.alert("hello from js file");
+document.write("<h1>Hello from JS</h1>");
+console.log("hello from js");
+console.error("'error from js");
+console.table({ ahmed: "goo", ali: "mohamed" });
+console.log(
+  "hello from %cJS %file",
+  "color: red; font-size:40px",
+  "color:blue; font-size:20px",
+);
+my_name = "osama";
+console.log("hello " + my_name);
+console.log("hello" + my_name);
+console.log(typeof [10, 15, 17]);
+console.log(typeof { name: "osama", age: 20, country: "egypt" });
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
+hello.innerHTML = "option";
+document.write("a + b");
+let a = "we love";
+let b = "javascript";
+let c = "and";
+let d = "programming";
+console.log(a + " " + b + " " + c + " " + d);
+console.log(10 + "osama");
+console.log(10 % 2);
+console.log(10 ** 2);
+console.log((100).toString());
+
