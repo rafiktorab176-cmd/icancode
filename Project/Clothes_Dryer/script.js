d7b09b7a743227a9fc138e62f4078e550713a002
Rafik <rafiktorab176@gmail.com>
2026-07-29 18:17:38 +0300

    new

--- Project/Clothes_Dryer/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const linksContainer = document.getElementById("links-container");
-
-hamburger.addEventListener("click", function () {
-    hamburger.classList.toggle("active");
-    linksContainer.classList.toggle("active");
-});
--- Project/blog/script.js
+++ Project/blog/script.js
@@ -1,10 +1,3 @@
-const hamburger = document.getElementById("hamburger");
-const linksContainer = document.getElementById("links-container");
-
-hamburger.addEventListener("click", function () {
-    hamburger.classList.toggle("active");
-    linksContainer.classList.toggle("active");
-});
 // قاعدة بيانات نصوص المقالات الكاملة
 const articlesData = {
     "article-fridge": `
