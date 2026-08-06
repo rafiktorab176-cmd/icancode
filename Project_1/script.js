6001f8009f56f65a2da461253acdc424f0fe2415
Rafik <rafiktorab176@gmail.com>
2026-08-06 19:33:19 +0300

    .,.

--- Project_1/script.js
+++ Project_1/script.js
@@ -637,3 +637,18 @@ document.addEventListener("DOMContentLoaded", () => {
   if (searchInput) searchInput.addEventListener("input", filterCodes);
   if (applianceFilter) applianceFilter.addEventListener("change", filterCodes);
 });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    const targetId = this.getAttribute("href");
+    if (targetId === "#" || targetId === "") return;
+
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      e.preventDefault();
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+      });
+      history.pushState(null, null, " ");
+    }
+  });
+});
