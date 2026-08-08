1ddec1021ee78717550c4aeb5d274e8bed5e45f9
Rafik <rafiktorab176@gmail.com>
2026-08-09 01:33:35 +0300

    زو

---
+++ lesson_3/main.js
@@ -0,0 +1,22 @@
+// window.alert("Hello, world!");
+// this.alert("hello, world!");
+// alert("Hello, world!");
+// let confirmmsg = confirm("Are you sure?");
+// console.log(confirmmsg);
+// if (confirmmsg === true) {
+//   console.log("item deleted");
+// } else {
+//   console.log("item not deleted");
+// }
+// const promptmsg = prompt("what is your name?");
+// console.log(promptmsg);
+// setTimeout(function () {
+//   console.log("msg");
+// }, 3000);
+// function saymessage() {
+//   console.log(" i'm Message");
+// }
+setTimeout(saymessage, 3000);
+function saymessage() {
+  console.log(" i'm Message for osama");
+}
